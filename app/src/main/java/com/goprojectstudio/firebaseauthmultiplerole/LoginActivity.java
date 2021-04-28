de626e1b887a79bf6196af942a06f0aef0de1add
adiirwanto <irwantoadi926@gmail.com>
2021-04-29 05:34:30 +0700

    connect to firebase

--- app/src/main/java/com/goprojectstudio/firebaseauthmultiplerole/LoginActivity.java
+++ app/src/main/java/com/goprojectstudio/firebaseauthmultiplerole/LoginActivity.java
@@ -2,13 +2,44 @@ package com.goprojectstudio.firebaseauthmultiplerole;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonRegister = findViewById(R.id.btn_register_page);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+//            reload();
+        }
     }
 }
--- app/src/main/java/com/goprojectstudio/firebaseauthmultiplerole/RegisterActivity.java
+++ app/src/main/java/com/goprojectstudio/firebaseauthmultiplerole/RegisterActivity.java
@@ -3,12 +3,23 @@ package com.goprojectstudio.firebaseauthmultiplerole;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button buttonBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        buttonBack = findViewById(R.id.btn_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
